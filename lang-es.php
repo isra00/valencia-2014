e745a2db229858080e057e4f4f33e90b15c48355
isra00 <isra00@gmail.com>
2014-05-21 20:43:24 +0200

    Fichero olvidado en el anterior commit

---
+++ lang-es.php
@@ -0,0 +1,22 @@
+<?php
+
+$msg['page_title']		= 'Ver en directo el Encuentro Vocacional del Camino Neocatecumenal Valencia 2014';
+$msg['meta_content']	= 'Retransmisión en directo del Encuentro Vocacional de jóvenes y familias del Camino Neocatecumenal en Valencia 2014. Kiko Argüello.';
+$msg['meta_site_name']	= 'Rio 2013 Caminho Neocatecumenal';
+
+$msg['chrome_frame']	= 'Estás usando un navegador <strong>obsoleto</strong>. Por favor, <a href="http://browsehappy.com/">actualiza tu navegador</a> o <a href="http://www.google.com/chromeframe/?redirect=true">activa Google Chrome Frame</a> para mejorarlo.';
+
+$msg['title']			= 'Encuentro Vocacional del Camino Neocatecumenal';
+$msg['subtitle_start']	= '1 de junio';
+$msg['intro_text']		= 'Vea en directo el <strong>Encuentro de jóvenes y familias</strong> en Valencia con los iniciadores y responsables del Camino Neocatecumenal.';
+
+$msg['streaming_help']	= '¿No puede ver el vídeo correctamente? <a href="">Haga click aquí</a>.';
+
+$msg['not_yet_title']	= 'La retransmisión comenzará el 1 de junio a las 16:30, hora de España';
+$msg['local_time']		= 'En su hora local serán las ';
+$msg['finished']		= 'El encuentro ha terminado';
+$msg['unavailable']		= 'La retransmisión no está disponible en estos momentos. Disculpe las molestias.';
+
+$msg['other_media']		= 'Otros medios que también emiten el encuentro';
+
+$msg['link_cnc'] 		= 'Sitio oficial <strong>Camino Neocatecumenal</strong>';
