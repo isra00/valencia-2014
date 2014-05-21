d42b05525b745d199cd37409f0dfa17e4ce9bc7f
isra00 <isra00@gmail.com>
2014-05-21 21:33:09 +0200

    Corregida traducción EN

--- lang-en.php
+++ lang-en.php
@@ -1,18 +1,18 @@
 <?php
 
-$msg['page_title']		= 'Vocational Encounter of the Neocatechumenal Way – live transmission';
-$msg['meta_content']	= 'Live transmission of the vocational encounter with the youth of the Neocatechumenal Way for WYD 2013 Rio de Janeiro. Kiko Argüello.';
-$msg['meta_site_name']	= 'Rio 2013 Neocatechumenal Way';
+$msg['page_title']		= 'Vocational Encounter of the Neocatechumenal Way - watch live';
+$msg['meta_content']	= 'Live transmission of the vocational encounter with the youth and families of the Neocatechumenal Way Valencia 2014. Kiko Argüello.';
+$msg['meta_site_name']	= 'Neocatechumenal Way Official';
 
 $msg['chrome_frame']	= 'You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.';
 
 $msg['title']			= 'Vocational encounter of the Neocatechumenal Way';
-$msg['subtitle_start']	= 'July 29th';
-$msg['intro_text']		= 'Watch live the <strong>meeting with the youth</strong> in Brasil with Kiko Argüello, Carmen Hernández and Fr. Mario Pezzi, initiators of the Neocatechumenal Way, for World Youth Day.';
+$msg['subtitle_start']	= 'June 1st';
+$msg['intro_text']		= 'Watch live the <strong>meeting with the youth and families</strong> in Valencia with the initiators and responsibles of the Neocatechumenal Way.';
 
 $msg['streaming_help']	= 'Having trouble viewing the video? <a href="">Click here</a>.';
 
-$msg['not_yet_title']	= 'The event will be transmitted on July 29th at 2:00 PM local Rio time (GMT-3).';
+$msg['not_yet_title']	= 'The transmission will start on June 1st at 16:30 PM local Spanish time';
 $msg['local_time']		= 'In your local time zone, the time will be ';
 $msg['finished']		= 'The meeting has finished.';
 $msg['unavailable']		= 'The stream is not available at this time. We apologize for any inconvenience.';
